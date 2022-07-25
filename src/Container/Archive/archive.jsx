5c3646b216fe0d70b22bebf5871f3eec14c27013
Klint Keown <klintkeown@gmail.com>
2022-07-24 21:27:18 -0500

    router setup

---
+++ src/Container/Archive/archive.jsx
@@ -0,0 +1,7 @@
+export default function Archive() {
+    return (
+        <main style={{ padding: "1rem 0" }}>
+            <h2>Archive</h2>
+        </main>
+    );
+}
--- src/index.js
+++ src/index.js
@@ -1,17 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import App from "./App";
+import Home from "./Container/Home/home.jsx";
+import Archive from "./Container/Archive/archive.jsx";
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+
+const root = ReactDOM.createRoot(
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+root.render(
+  <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<App />}>
+        <Route path='/home' element={<Home />} />
+        <Route path='/archive' element={<Archive />} />
+        <Route
+          path="*"
+          element={
+            <main style={{ padding: "1rem" }}>
+              <p>There's nothing here!</p>
+            </main>
+          }
+        />        
+      </Route>
+    </Routes>
+  </BrowserRouter>
+);
