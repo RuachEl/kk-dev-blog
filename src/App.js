83a0f89aa9ed6b662e500cfa4a5ec40a4d42de19
Klint Keown <klintkeown@gmail.com>
2022-08-11 18:37:10 -0500

    adding reactStrap

--- src/App.js
+++ src/App.js
@@ -1,20 +1,29 @@
 import { Link } from "react-router-dom";
+import { Container, Row, Col } from "reactstrap";
 
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 export default function App() {
   return (
     <div>
-      <h1>Klint's Dev Blog</h1>
-      <nav
-        style={{
-          borderBottom: "solid 1px",
-          paddingBottom: "1rem",
-        }}
-      >
-        <Link to="/">Home</Link> |{" "}
-        <Link to="/archive">Archive</Link>
-      </nav>
+      <Container>
+        <Row>
+          <Col xs='2' className=".App__logo">
+            <img />
+          </Col>
+          <Col xs='4' className=".App__heading">
+            <h1>Klint's Dev Blog</h1>
+          </Col>
+          <Col xs='6' className=".App__links">
+            <nav>
+              <Link to="/">Home</Link> |{" "}
+              <Link to="/archive">Archive</Link>
+            </nav>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
